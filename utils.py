e959dd9fe1dfd0ffa6005955ade003291e8ad920
HudzenkoVlad <hudzenkovladislav@knu.ua>
2024-02-07 13:32:17 +0200

    FiveDegreeCommit

--- utils.py
+++ utils.py
@@ -3,3 +3,12 @@ def Fact(n):
         return 1
     else:
         return n * Fact(n - 1)
+
+
+def FiveDegree(x):
+    if x == 1:
+        return "Число є степенем п'ятірки"
+    if x != int(x):
+        return "Число не є степенем п'ятірки"
+    else:
+        return FiveDegree(x / 5)
